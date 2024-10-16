44f09fb01d0876f7eeb8966bdc1caab05186bb63
KimHS17 <skes01149@naver.com>
2024-10-16 10:29:24 +0900

    react Hook

--- react/hooks-tutorial/src/Components/Info.js
+++ react/hooks-tutorial/src/Components/Info.js
@@ -1,8 +1,15 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Info = () => {
   const [name, setName] = useState("");
   const [nickname, setNickname] = useState("");
+
+  useEffect(() => {
+    console.log("effect");
+    return () => {
+      console.log("cleanup");
+    };
+  }, []);
 
   const onChangeName = (e) => {
     setName(e.target.value);
