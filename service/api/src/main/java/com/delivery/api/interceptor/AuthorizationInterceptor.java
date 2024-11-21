3d3e33f9d6f3f7fd776d56eee43ed9517bfc40be
KimHS17 <skes01149@naver.com>
2024-11-21 11:01:17 +0900

    JWT 발행 및 인증

--- service/api/src/main/java/com/delivery/api/interceptor/AuthorizationInterceptor.java
+++ service/api/src/main/java/com/delivery/api/interceptor/AuthorizationInterceptor.java
@@ -1,18 +1,28 @@
 package com.delivery.api.interceptor;
 
+import com.delivery.api.common.error.ErrorCode;
+import com.delivery.api.common.error.TokenErrorCode;
+import com.delivery.api.common.exception.ApiException;
+import com.delivery.api.domain.token.business.TokenBusiness;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.resource.ResourceHttpRequestHandler;
+
+import java.util.Objects;
 
 @Slf4j
 @RequiredArgsConstructor
 @Component
 public class AuthorizationInterceptor implements HandlerInterceptor {
+    private final TokenBusiness tokenBusiness;
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         log.info("Authorization Interceptor url : {}", request.getRequestURL());
@@ -27,8 +37,22 @@ public class AuthorizationInterceptor implements HandlerInterceptor {
             return true;
         }
 
-        // TODO: header 검증
+        // header 검증
+        var accessToken = request.getHeader("authorization-token");
+        if(accessToken == null) {
+            throw new ApiException(TokenErrorCode.AUTHORIZATION_TOKEN_NOT_FOUND);
+        }
 
-        return true; // 일단 통과 처리
+        var userId = tokenBusiness.validationAccessToken(accessToken);
+
+        if(userId != null) {
+            // context에 저장
+            var requestContext = Objects.requireNonNull(RequestContextHolder.getRequestAttributes());
+            requestContext.setAttribute("userId", userId, RequestAttributes.SCOPE_REQUEST);
+
+            return true; // 유효한 경우 통과 처리
+        }
+
+        throw new ApiException(ErrorCode.BAD_REQUEST, "인증 실패");
     }
 }
